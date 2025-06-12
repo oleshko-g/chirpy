4621bf196a7b0d17c22f31da8c97294b66911258
Gennady Oleshko <gennady@oleshko-g.ru>
2025-06-12 19:40:54 +0300

    add logo.png

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = "8080"
+const rootPath = "./public"
+
+func newServeMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(rootPath)))
+	return mux
+}
 
 func main() {
-	server := http.Server{
-		Handler: http.NewServeMux(),
-		Addr:    ":8080",
-	}
-	defer server.Close()
 
-	for {
-		server.ListenAndServe()
+	server := &http.Server{
+		Handler: newServeMux(),
+		Addr:    ":" + port,
 	}
+
+	fmt.Printf("Serving on the port %s", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
