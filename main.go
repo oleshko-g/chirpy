acfee1ef81abb58da3b4e4ce9a7a6a24cb4a79ab
Gennady Oleshko <gennady@oleshko-g.ru>
2025-06-14 13:20:12 +0300

    implement /metics & /reset

--- main.go
+++ main.go
@@ -4,18 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port string = "8080"
 const rootPath = "./public"
 
+var c *apiConfig = &apiConfig{
+	fileserverHits: atomic.Int32{},
+}
+
 func newServeMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.Handle("/app/",
-		http.StripPrefix("/app",
-			http.FileServer(http.Dir(rootPath))))
+		c.incFileSrvHits(
+			http.StripPrefix("/app",
+				http.FileServer(
+					http.Dir(rootPath),
+				),
+			),
+		),
+	)
 
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", c.showFileSrvHits)
+	mux.HandleFunc("/reset", c.resetFileSrvHits)
 	return mux
 }
 
--- server.go
+++ server.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"sync/atomic"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+}
+
+func (c *apiConfig) incFileSrvHits(h http.Handler) http.Handler {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits.Add(1)
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(handler)
+}
+
+func (c *apiConfig) resetFileSrvHits(w http.ResponseWriter, _ *http.Request) {
+	c.fileserverHits.Store(0)
+	w.Header().Add("content-type", "text/plain; charset=utf-8")
+	fileServerHits := strconv.Itoa(int(c.fileserverHits.Load()))
+	responseData := []byte("Hits have been reset to: " + fileServerHits)
+	w.Write(responseData)
+}
+
+func (c *apiConfig) showFileSrvHits(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("content-type", "text/plain; charset=utf-8")
+	fileServerHits := strconv.Itoa(int(c.fileserverHits.Load()))
+	responseData := []byte("Hits: " + fileServerHits)
+	w.Write(responseData)
 }
 
 func healthzHandler(w http.ResponseWriter, _ *http.Request) {
