8a54093244553cf26c21388b1ef17c6a0e0911ad
Gennady Oleshko <gennady@oleshko-g.ru>
2025-07-01 10:02:09 +0300

    add signUserJWT(), validateUserJWT()

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.39.0
 )
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+
+	jwt "github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 
@@ -14,4 +20,33 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func signUserJWT(userID uuid.UUID, jwtSecret string, expiresAfter time.Duration) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{
+			Issuer:    "chirpy",
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresAfter)),
+			Subject:   userID.String(),
+		},
+	)
+	return t.SignedString([]byte(jwtSecret))
+}
+
+func validateUserJWT(tokenString, jwtSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	userJWT, err := jwt.ParseWithClaims(tokenString, &claims,
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(jwtSecret), nil
+		},
+	)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	id, err := userJWT.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return uuid.Parse(id)
 }
