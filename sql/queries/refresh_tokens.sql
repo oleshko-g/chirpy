354b43e6343cfc69bf731b51e61741c57bdc06e4
Gennady Oleshko <gennady@oleshko-g.ru>
2025-07-06 20:20:38 +0300

    add InsertRefreshToken()

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,10 @@
+-- name: InsertRefreshToken :exec
+INSERT INTO
+    refresh_tokens (
+        token,
+        created_at,
+        updated_at,
+        user_id,
+        expires_at
+    )
+VALUES ($1, NOW(), NOW(), $2, $3);
